646856f354ba432e6ad158e6eff54ee2e870bf15
amine kammoun <aminekammoun55@gmail.com>
2021-06-06 04:47:03 +0100

    products

--- src/components/production/production.vue
+++ src/components/production/production.vue
@@ -1,17 +1,35 @@
 <template>
-  <v-main>
-    <v-container>
-      <v-row>
-        <v-col cols="6">
-          <v-card height="200"></v-card>
-        </v-col>
-        <v-col cols="6">
-          <div>
-              
-          </div>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-main>
+  <v-container>
+    <v-row style="    padding: 107px;">
+      <v-col
+        cols="5"
+        style="height:400px;width:200px; border: solid 1px; border-radius: 10px;"
+      >
+      </v-col>
+      <v-col cols="2"> </v-col>
+      <v-col
+        cols="5"
+        style="height:400px; border: solid 1px; border-radius: 10px"
+      >
+        <v-img
+          style="    width: 286px;
+    position: relative;
+    left: 14%;
+"
+          src="../../assets/images/custumer.png"
+        />
+        <h1>Entreprise</h1>
+
+        <v-img src="../../assets/images/chatbubbles.png" width="50px" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-<style></style>
+<style scoped>
+h1 {
+  text-align: center;
+  color: #bc6c25;
+  font-weight: bold;
+  padding-top: 10px;
+}
+</style>
